81eadace6b0c447b7e5c9da3859bbd1810b6ce2c
rylanharper <rylanharper@gmail.com>
2022-01-29 16:46:11 -0800

    feat: updated dependencies and shopify graphql version

--- web/src/graphql/graphql.js
+++ web/src/graphql/graphql.js
@@ -1,6 +1,6 @@
 import { GraphQLClient } from 'graphql-request'
 
-const endpoint = `https://${process.env.GRIDSOME_SHOPIFY_STOREFRONT}.myshopify.com/api/2021-10/graphql.json`
+const endpoint = `https://${process.env.GRIDSOME_SHOPIFY_STOREFRONT}.myshopify.com/api/2022-01/graphql.json`
 
 export default {
   install (Vue, options) {
