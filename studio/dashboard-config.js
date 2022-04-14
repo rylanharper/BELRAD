4adb0dfa8e83d44270638c6b0249ecd68e81931b
rylanharper <rylanharper@gmail.com>
2022-04-14 16:44:59 -0700

    feat: added in example netlify deploy

--- studio/dashboard-config.js
+++ studio/dashboard-config.js
@@ -1,10 +1,20 @@
 export default {
   widgets: [
     {
-      name: 'project-users',
-      layout: {
-        width: 'medium',
-        height: 'small'
+      name: 'netlify',
+      options: {
+        title: 'Website Deployment',
+        description:
+          'NOTE: Because this is a statically built website, it needs to be re-deployed to see changes made when documents are published.',
+        sites: [
+          {
+            title: 'BELRAD',
+            apiId: process.env.SANITY_STUDIO_NETLIFY_API_ID,
+            buildHookId: process.env.SANITY_STUDIO_NETLIFY_BUILD_HOOK_ID,
+            name: process.env.SANITY_STUDIO_FRONTEND_NAME,
+            url: process.env.SANITY_STUDIO_FRONTEND_URL
+          }
+        ]
       }
     },
     {
