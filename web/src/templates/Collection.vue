de9106dfd38e03c0e0594531db59f00e5a791c23
rylanharper <rylanharper@gmail.com>
2022-05-03 14:03:39 -0700

    chore: removed unused filter code

--- web/src/templates/Collection.vue
+++ web/src/templates/Collection.vue
@@ -9,7 +9,6 @@
       </div>
       <filter-and-sort :products="allProducts" :showing-sort="showingSort" :showing-filter="showingFilter">
         <template v-slot="filterData">
-          <!-- <div>Items:{{ filterData.filteredProducts.length }}</div> -->
           <div class="product-grid">
             <div
               v-for="product in filterData.filteredProducts"
@@ -21,41 +20,6 @@
           </div>
         </template>
       </filter-and-sort>
-      <!-- <div class="collection__nav">
-        <h1>{{ collection.title }} — {{ collection.products.length }} Items</h1>
-        <button @click="showFilterMenu = !showFilterMenu">
-          Filter {{ showFilterMenu === true ? '-' : '+' }}
-        </button>
-      </div>
-
-      <transition
-        appear
-        mode="out-in"
-        @before-enter="beforeEnter"
-        @enter="enter"
-        @leave="leave"
-        :css="false"
-      >
-        <div v-show="showFilterMenu" class="collection__filter-menu">
-          <div v-for="filter in listOfFilters" :key="filter.id" class="filter">
-            <span class="filter-name">{{ filter.name }}</span>
-            <label v-for="(option, o) in filter.options" :key="o">
-              <input v-model="filters[filter.name]" :value="option" type="checkbox" />
-              <span>{{ option }}</span>
-            </label>
-          </div>
-        </div>
-      </transition>
-
-      <div class="product-grid">
-        <div
-          v-for="product in filteredProducts"
-          :key="product.id"
-          class="product-grid__product group"
-        >
-          <product-card :product="product" />
-        </div>
-      </div> -->
     </section>
   </Layout>
 </template>
