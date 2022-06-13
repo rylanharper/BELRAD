77703c8fcb578302ecabe7faf7bfb119144d060d
rylanharper <rylanharper@gmail.com>
2022-06-13 13:26:35 -0700

    chore: update graphql endpoint

--- web/src/graphql/graphql.js
+++ web/src/graphql/graphql.js
@@ -1,9 +1,9 @@
 import { GraphQLClient } from 'graphql-request'
 
-const endpoint = `https://${process.env.GRIDSOME_SHOPIFY_STOREFRONT}.myshopify.com/api/2022-01/graphql.json`
+const endpoint = `https://${process.env.GRIDSOME_SHOPIFY_STOREFRONT}.myshopify.com/api/2022-04/graphql.json`
 
 export default {
-  install (Vue, options) {
+  install (Vue) {
     Vue.prototype.$graphql = new GraphQLClient(endpoint, {
       headers: {
         'X-Shopify-Storefront-Access-Token': 
