418ebd5f137de7a6e27b13db25a89acf404ed603
rylanharper <rylanharper@gmail.com>
2022-06-24 13:03:16 -0700

    feat: update to main windi config

--- web/windi.config.js
+++ web/windi.config.js
@@ -1,18 +1,16 @@
 module.exports = {
   darkMode: 'class',
   theme: {
-    extend: {
-      screens: {
-        'sm': '640px',
-        'md': '768px',
-        'lg': '1024px',
-        'xl': '1280px',
-        '2xl': '1536px'
-      },
-      fontFamily: {
-        sans: ['degular', 'sans-serif'],
-        mono: ['ballinger-mono', 'monospace'],
-      }
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px'
+    },
+    fontFamily: {
+      sans: ['degular', 'sans-serif'],
+      mono: ['ballinger-mono', 'monospace'],
     }
   },
   plugins: [
